24166fc735d9abd7c6fdd45933cce371e177faef
Grupo 2 <gpallero88@gmail.com>
2023-07-27 15:23:49 -0300

    guide function for the imgs

--- props/challenge1/utils.js
+++ props/challenge1/utils.js
@@ -1,4 +1,5 @@
 // I take this and translated it to the script in 'passingPropsChallenge1.html'
+// Used this as a guide to make the function in 'passingPropsChallenge1.html'
 export function getImageUrl(imageId, size = 's') {
   return (
     'https://i.imgur.com/' +
