733c41459456af451f967892714df0727210dff4
Grupo 2 <gpallero88@gmail.com>
2023-08-02 22:17:35 -0300

    Realization, no js for this challenge

--- props/challenge3/challenge3.js
+++ props/challenge3/challenge3.js
@@ -1,27 +1,4 @@
 // Main functions for challenge3
-function Profile(){
-  return(
-    <div>
-      <div className="Card">
-        <div className="Card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="https://i.imgur.com/OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-
-      <div className="Card">
-        <div className="Card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistossomiasis.</p>
-        </div>
-      </div>
-
-    </div>
-  )
-}
+// After a few tries (actually ... a lot of tries)
+// I realized that I cannot use JS to return html elements
+// So I will finish the challenge without this js file
