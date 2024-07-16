73c2c778c9f2155b0cb7498468ea1e72c456866f
Guillermo Pallero <gpallero88@gmail.com>
2024-07-16 12:13:20 -0300

    Created challenge3.js it contains data used in challene3.html

---
+++ addingInteractivity/managingState/preservingAndResettingState/challenges/challenge3.js
@@ -0,0 +1,5 @@
+const initialContacts = [
+    { id: 0, name: "Taylor", email: "taylor@email.com"},
+    { id: 1, name: "Alice", email: "alice@email.com"},
+    { id: 2, name: "Bob", email: "bob@email.com"}
+]
