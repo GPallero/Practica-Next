c186baf64d38bdee66055392570fa234669aa8af
Guillermo Pallero <gpallero88@gmail.com>
2024-07-18 11:55:31 -0300

    Added id's in challenge4.js to use them as a keys, linked style sheets to challenge4.html.

--- addingInteractivity/managingState/preservingAndResettingState/challenges/challenge4.js
+++ addingInteractivity/managingState/preservingAndResettingState/challenges/challenge4.js
@@ -1,23 +1,30 @@
 const images = [
     {
+        id: 0,
         place: "Penang, Malaysia",
         src: "https://i.imgur.com/FJeJR8M.jpg"
     }, {
+        id: 1,
         place: "Lisbon, Portugal",
         src: "https://i.imgur.com/dB2LRbj.jpg"
     }, {
+        id: 2,
         place: "Bilbao, Spain",
         src: "https://i.imgur.com/z08o2TS.jpg"
     }, {
+        id: 3,
         place: "Valparaíso, Chile",
         src: "https://i.imgur.com/Y3utgTi.jpg"
     }, {
+        id: 4,
         place: "Schwyz, Switzerland",
         src: "https://i.imgur.com/JBbMpWY.jpg"
     }, {
+        id: 5,
         place: "Prague, Czechia",
         src: "https://i.imgur.com/QwUKKmF.jpg"
     }, {
+        id: 6,
         place: "Ljubljana, Slovenia",
         src: "https://i.imgur.com/3aIiwfm.jpg"
     }
