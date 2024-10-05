27577d117476fcdea57db150a147b894b04b2d48
Guillermo Pallero <gpallero88@gmail.com>
2024-10-05 20:10:38 -0300

    Fixed contacts data.

--- addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge1.js
+++ addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge1.js
@@ -1,6 +1,6 @@
 const contacts = [
     {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
-    {id: 1, name: 'Alice', enail: 'alice@email.com'},
+    {id: 1, name: 'Alice', email: 'alice@email.com'},
     {id: 2, name: 'Bob', email: 'bob@email.com'},
 ];
 
