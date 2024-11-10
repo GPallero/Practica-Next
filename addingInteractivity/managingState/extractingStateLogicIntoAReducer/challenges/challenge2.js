e84241629298157d70793a0891a193f7db2e8ece
Guillermo Pallero <gpallero88@gmail.com>
2024-11-10 14:29:31 -0300

    Created 'challenge2.j'

---
+++ addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge2.js
@@ -0,0 +1,5 @@
+const contacts = [
+    {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
+    {id: 1, name: 'Alice', email: 'alice@email.com'},
+    {id: 2, name: 'Bob', email: 'bob@email.com'},
+];
