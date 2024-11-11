eb2c0ab4066777009764d98c0e2ba3fe07b67499
Guillermo Pallero <gpallero88@gmail.com>
2024-11-10 23:11:01 -0300

    Added 'messengerReducer' function in challenge2.html and 'initialState' in challenge2.js

--- addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge2.js
+++ addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge2.js
@@ -1,3 +1,8 @@
+const initialState = {
+    selectedId: 0,
+    message: 'Hello',
+};
+
 const contacts = [
     {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
     {id: 1, name: 'Alice', email: 'alice@email.com'},
