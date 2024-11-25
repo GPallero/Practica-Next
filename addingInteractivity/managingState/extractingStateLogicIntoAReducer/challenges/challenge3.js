1d3b0a5a98a3a3553b90fc27fc73f91030938716
Guillermo Pallero <gpallero88@gmail.com>
2024-11-25 11:09:20 -0300

    Created initialState & contacts data.

---
+++ addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge3.js
@@ -0,0 +1,10 @@
+const initialState = {
+    selectedId: 0,
+    message: 'Hello',
+};
+
+const contacts = [
+    {id: 0, name: 'Taylor', email: 'taylor@mail.com'},
+    {id: 1, name: 'Alice', email: 'alice@mail.com'},
+    {id: 2, name: 'Bob', email: 'bob@mail.com'},
+];
