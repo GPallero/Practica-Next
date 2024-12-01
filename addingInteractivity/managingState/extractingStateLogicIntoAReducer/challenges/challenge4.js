cd9fb5dfaeafa485778115ec3d87930c272d64ca
Guillermo Pallero <gpallero88@gmail.com>
2024-12-01 16:49:57 -0300

    Created challenge4.js which contains data used to solve challenge4.html

---
+++ addingInteractivity/managingState/extractingStateLogicIntoAReducer/challenges/challenge4.js
@@ -0,0 +1,14 @@
+const initialState = {
+    selectedId: 0,
+    messages: {
+        0: 'Hello Taylor',
+        1: 'Hello Alice',
+        2: 'Hello Bob',
+    }
+}
+
+const contacts = [
+    {id: 0, name: "Taylor", email:"taylor@email"},
+    {id: 1, name: "Alice", email:"alice@email"},
+    {id: 2, name: "Bob", email:"bob@email"},
+];
